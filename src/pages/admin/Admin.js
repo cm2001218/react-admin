7f227d88eaae91ed5689bcfbc06a24d91ee1cf92
unknown <cm2001218@gmail.com>
2020-10-26 15:49:31 +1300

    delete comment in admin.js

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -51,35 +51,3 @@ export default class Admin extends Component {
     );
   }
 }
-
-// export default class Admin extends Component {
-//   render() {
-//     const user = memoryUtils.user;
-//     // 如果内存没有存储user ==> 当前没有登陆
-//     if (!user || !user._id) {
-//       // 自动跳转到登陆(在render()中)
-//       return <Redirect to="/login" />;
-//     }
-//     return (
-//       <Layout style={{ minHeight: "100%" }}>
-//         <Sider>
-//           <LeftNav />
-//         </Sider>
-//         <Layout>
-//           <Header>Header</Header>
-//           <Content style={{ margin: 20, backgroundColor: "#fff" }}>
-//             <Switch>
-//               {/* <Redirect from="/" exact to="/home" /> */}
-//               <Route path="/home" component={Home} />
-//               <Route path="/category" component={Category} />
-//               <Route path="/product" component={Product} />
-//               <Route path="/user" component={User} />
-//               <Route path="/role" component={Role} />
-//             </Switch>
-//           </Content>
-//           <Footer style={{ textAlign: "center", color: "#cccccc" }}>推荐使用谷歌浏览器，可以获得更佳页面操作体验</Footer>
-//         </Layout>
-//       </Layout>
-//     );
-//   }
-// }
